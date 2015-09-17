32e8ad896a0c60dbea09b4f9d973feced3332375
Martin Delille <martin@phonations.com>
2015-09-17 12:48:15 +0200

    install automatically Vundle vim plugin

--- bin/dotfiles_setup.sh
+++ bin/dotfiles_setup.sh
@@ -4,3 +4,7 @@ ln -s ~/.dotfiles/zshenv ~/.zshenv
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim ~/.vim
+
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
