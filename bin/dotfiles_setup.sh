c847b19d0915ae32f3ec990a5ab021cf568fccb3
Martin Delille <martin@phonations.com>
2015-09-17 13:15:54 +0200

    add oh-my-zsh install

--- bin/dotfiles_setup.sh
+++ bin/dotfiles_setup.sh
@@ -5,6 +5,9 @@ ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim ~/.vim
 
+# Install Oh my Zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
