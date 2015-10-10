9f0458d28266e70879db812d0e6b1aac866e5dc1
Martin Delille <martin@phonations.com>
2015-10-10 13:29:31 +0200

    common gitignore file

--- bin/dotfiles_setup.sh
+++ bin/dotfiles_setup.sh
@@ -2,6 +2,7 @@
 
 ln -s ~/.dotfiles/zshenv ~/.zshenv
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim ~/.vim
 
