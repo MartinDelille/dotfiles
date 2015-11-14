84d69376a964b3a54b998b8b0768a21747dfaccd
Martin Delille <martin@phonations.com>
2015-11-14 18:58:03 +0100

    change oh my zsh install order

--- bin/dotfiles_setup.sh
+++ bin/dotfiles_setup.sh
@@ -11,9 +11,10 @@ ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim ~/.vim
 
-# Install Oh my Zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+# Install Oh my Zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
