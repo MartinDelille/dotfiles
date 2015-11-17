9fea9c438c81bee120c8659ffa1d3a0feaae5622
Martin Delille <martin@phonations.com>
2015-11-17 13:46:50 +0100

    remove ln ambiguity

--- bin/dotfiles_setup.sh
+++ bin/dotfiles_setup.sh
@@ -17,7 +17,7 @@ ln -s ~/.dotfiles/zshenv ~/.zshenv
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/vimrc ~/.vimrc
-ln -s ~/.dotfiles/vim ~/.vim
+ln -s ~/.dotfiles/vim/ ~/.vim/
 
 if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
   echo "### Install Vundle ###"
