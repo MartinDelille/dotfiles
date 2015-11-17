e4f85faa094895495f23fca5cd93457f5dad33f2
Martin Delille <martin@phonations.com>
2015-11-17 13:02:10 +0100

    add echo

--- bin/dotfiles_setup.sh
+++ bin/dotfiles_setup.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" = linux* ]]; then
+  echo "### Linux specific: Install Git, Vim and ZSH ###"
   sudo apt-get install -y git vim zsh
-  if [[ ! -d ~/.dotfiles ]]; then
-    git clone https://github.com/MartinDelille/dotfiles ~/.dotfiles
-  fi
 fi
 
+if [[ ! -d ~/.dotfiles ]]; then
+  echo "### Cloning dotfiles ###"
+  git clone https://github.com/MartinDelille/dotfiles ~/.dotfiles
+fi
+
+echo "### Linking configuration ###"
 ln -s ~/.dotfiles/zshenv ~/.zshenv
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim ~/.vim
 
-# Install Vundle
 if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
+  echo "### Install Vundle ###"
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 vim +PluginInstall +qall
 
-# Install Oh my Zsh
+echo "### Install Oh my Zsh ###"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
