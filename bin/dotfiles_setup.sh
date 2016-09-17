b07e2fb628f183feee4ce5e2b59d4a2c61134379
Martin Delille <martin@phonations.com>
2016-09-17 19:27:36 +0200

    +qall

--- bin/dotfiles_setup.sh
+++ bin/dotfiles_setup.sh
@@ -30,7 +30,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "### Install Vim plugin ###"
-vim +PlugInstall
+vim +PlugInstall +qall
 
 echo "### Install Powerline font"
 if [[ "$OSTYPE" = linux* ]]; then
