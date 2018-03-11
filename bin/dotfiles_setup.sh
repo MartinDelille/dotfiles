ef5cb5b6bc5e4c06fc8cc509022d5a625a57c49f
Martin Delille <martin@phonations.com>
2018-03-11 18:19:33 +0100

    remove vim folders

--- bin/dotfiles_setup.sh
+++ bin/dotfiles_setup.sh
@@ -19,7 +19,7 @@ ln -s ~/.dotfiles/zshenv ~/.zshenv
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/vimrc ~/.vimrc
-ln -s ~/.dotfiles/vim/ ~/.vim/
+ln -s ~/.dotfiles/ftplugin ~/.vim/ftplugin
 if [[ "$OSTYPE" = darwin* ]]; then
     ln -s ~/.dotfiles/my.env.plist ~/Library/LaunchAgents/my.env.plist
 fi
--- vim/ftdetect/haxe.vim
+++
@@ -1,1 +0,0 @@
-au BufRead,BufNewFile *.hx set filetype=haxe
