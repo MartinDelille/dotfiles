129eb4600822c9abc233df008eb23037d7dd9b47
Martin Delille <martin@phonations.com>
2018-09-11 12:58:15 +0200

    modern syntax for ffmpeg video codec

--- bin/convert-to-mjpeg.sh
+++ bin/convert-to-mjpeg.sh
@@ -4,6 +4,6 @@ if [ "$#" -ne 4 ]; then
   echo "Illegal number of parameters"
   echo "Usage: convert-to-mjpeg input_file output_file frame_rate timestamp"
 else
-  ffmpeg -i $1 -vcodec mjpeg -q:v 5 -metadata timecode=$4 -r $3 $2
+  ffmpeg -i $1 -c:v mjpeg -q:v 5 -metadata timecode=$4 -r $3 $2
 fi
 
