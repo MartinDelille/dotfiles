d62612b7495281eb0d4dcbae6a586faae4f02322
Martin Delille <martin@delille.org>
2021-03-07 23:43:08 +0100

    Improve zsh config

--- zsh/Darwin.zsh
+++ zsh/Darwin.zsh
@@ -3,9 +3,10 @@ export GIT_CREDENTIAL=osxkeychain
 alias ff="mdfind -onlyin ."
 alias kw="say kowabounga"
 alias wifi="networksetup -setairportpower airport"
-alias qt="open $HOME/Qt/Qt\ Creator.app"
+alias q="open $HOME/dev/Qt/Qt\ Creator.app"
 alias b="brew"
 alias bo="HOMEBREW_NO_AUTO_UPDATE=1 pgs && brew"
+alias qtlog="vi ~/Library/Preferences/QtProject/qtlogging.ini"
 
 #eval "$(docker-machine env default)"
 
@@ -13,3 +14,7 @@ alias bo="HOMEBREW_NO_AUTO_UPDATE=1 pgs && brew"
 export JAVA_HOME=`/usr/libexec/java_home`
 export PATH=${JAVA_HOME}/bin:$PATH
 export MAVEN_OPTS="-Xmx512m"
+
+function pbc {
+  cat $1 | pbcopy
+}
