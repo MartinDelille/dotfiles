d7fdc5e6a27793b9dd7b1ab33e2d0b5381203410
Martin Delille <martin@delille.org>
2021-06-25 15:22:14 +0200

    Move alias to Darwin.zsh

--- zsh/Darwin.zsh
+++ zsh/Darwin.zsh
@@ -3,11 +3,17 @@ export GIT_CREDENTIAL=osxkeychain
 alias ff="mdfind -onlyin ."
 alias kw="say kowabounga"
 alias wifi="networksetup -setairportpower airport"
+alias n="open ~/Dropbox/notes.md"
+alias po="p && open"
+alias chrome="open -a /Applications/Google\ Chrome.app"
+alias o="open"
+alias or="open -R"
 alias q="open $HOME/dev/Qt/Qt\ Creator.app"
 alias b="brew"
 alias bo="HOMEBREW_NO_AUTO_UPDATE=1 pgs && brew"
 alias qtlog="vi ~/Library/Preferences/QtProject/qtlogging.ini"
 alias grvc="git rev-parse --short head | tr -d '\n' | pbcopy"
+alias s="cd ~/Library/Application\ Support"
 
 #eval "$(docker-machine env default)"
 
