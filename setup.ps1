7d70c1b45cf1730b1fcde75b426ed66901e92a9f
Martin Delille <martin@delille.org>
2022-04-20 12:16:28 +0200

    Use diff-so-fancy from scoop

--- setup.ps1
+++ setup.ps1
@@ -5,4 +5,6 @@ New-Item -Force -ItemType SymbolicLink -Path ~/.vimrc -Value $root/vimrc
 New-Item -Force -ItemType SymbolicLink -Path ~/.gitconfig -Value $root/git/windows
 New-Item -Force -ItemType SymbolicLink -Path $Env:XDG_CONFIG_HOME/git -Value $root/git
 
+scoop install diff-so-fancy
+
 vim +PlugClean +PlugInstall +PlugUpdate +qall
