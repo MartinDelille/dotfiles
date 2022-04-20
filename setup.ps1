fdb279abbfb395c3197962e950e5cff77d1c2803
Martin Delille <martin@delille.org>
2022-04-20 17:09:40 +0200

    Fix git config conditional path on windows

--- setup.ps1
+++ setup.ps1
@@ -2,6 +2,7 @@ $root=$PSScriptRoot
 mkdir -Force -p ~/vimfiles/autoload
 New-Item -Force -ItemType SymbolicLink -Path ~/vimfiles/autoload/plug.vim -Value $root/vendors/vim-plug/plug.vim
 New-Item -Force -ItemType SymbolicLink -Path ~/.vimrc -Value $root/vimrc
-New-Item -Force -ItemType SymbolicLink -Path ~/.gitconfig -Value $root/git/config
+New-Item -Force -ItemType SymbolicLink -Path ~/.gitconfig -Value $root/git/windows
+New-Item -Force -ItemType SymbolicLink -Path $Env:XDG_CONFIG_HOME/git -Value $root/git
 
 vim +PlugClean +PlugInstall +PlugUpdate +qall
