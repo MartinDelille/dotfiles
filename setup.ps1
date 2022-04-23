2907b34f6423ad24c7b2ef173773fc8ce148513f
Martin Delille <martin@delille.org>
2022-04-23 13:24:48 +0200

    Install and import git-aliases and posh-git

--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,10 @@
-$root=$PSScriptRoot
 mkdir -Force -p ~/vimfiles/autoload
-New-Item -Force -ItemType SymbolicLink -Path ~/vimfiles/autoload/plug.vim -Value $root/vendors/vim-plug/plug.vim
-New-Item -Force -ItemType SymbolicLink -Path ~/.vimrc -Value $root/vimrc
-New-Item -Force -ItemType SymbolicLink -Path ~/.gitconfig -Value $root/git/windows
-New-Item -Force -ItemType SymbolicLink -Path $Env:XDG_CONFIG_HOME/git -Value $root/git
+New-Item -Force -ItemType SymbolicLink -Path ~/vimfiles/autoload/plug.vim -Value $PSScriptRoot/vendors/vim-plug/plug.vim
+New-Item -Force -ItemType SymbolicLink -Path ~/.vimrc -Value $PSScriptRoot/vimrc
+New-Item -Force -ItemType SymbolicLink -Path ~/.gitconfig -Value $PSScriptRoot/git/windows
+New-Item -Force -ItemType SymbolicLink -Path $Env:XDG_CONFIG_HOME/git -Value $PSScriptRoot/git
 
-scoop install diff-so-fancy
+scoop bucket add extras
+scoop install git-aliases posh-git diff-so-fancy
 
 vim +PlugInstall +PlugUpdate +qall
