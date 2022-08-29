4ea142a708d841a5e32e55f51e0cf84ff1f4ded9
Martin Delille <martin@delille.org>
2022-08-29 14:46:27 +0200

    Remove node from PATH

--- fish/config.fish
+++ fish/config.fish
@@ -24,7 +24,6 @@ set PATH /usr/local/opt/ruby/bin $PATH
 set PATH (gem environment gemdir)/bin $PATH
 set PATH $GOPATH/bin $PATH
 set PATH /usr/local/opt/python/libexec/bin $PATH
-set PATH /usr/local/opt/node@14/bin $PATH
 set -gx PATH ~/.cargo/bin $PATH
 
 # MacOS specifics
