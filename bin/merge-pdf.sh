e8eab849af5c924beb076507bc6f7a019daa9d5a
Martin Delille <martin@delille.org>
2022-09-10 10:53:14 +0200

    Improve merge-pdf.sh

--- bin/merge-pdf.sh
+++ bin/merge-pdf.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 echo "Merging PDF:"
 
+if [ "$#" -le 2 ]; then
+  echo "Please provide at least 3 arguments!"
+  echo "Usage: merge-pdf.sh <output.pdf> <input1.pdf> <input2.pdf> ..."
+  exit 1
+fi
+
 if [ "$#" -ge 8 ]; then
   echo "Doesn't handle more than 5 arguments!"
   echo "Usage: merge-pdf.sh <output.pdf> <input1.pdf> <input2.pdf> ..."
   exit 1
-else
-  #ffmpeg -i $1 -c:v mjpeg -q:v 5 -metadata timecode=$4 -r $3 $2
-  /usr/local/opt/ghostscript/bin/gs -q -sPAPERSIZE=letter -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=$1 $2 $3 $4 $5 $6 $7 $8
 fi
 
+/usr/local/opt/ghostscript/bin/gs -q -sPAPERSIZE=letter -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=$1 $2 $3 $4 $5 $6 $7 $8
