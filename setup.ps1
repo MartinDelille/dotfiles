34942f1b8c231cf087f14d19276c0b8f57f1a0a1
Martin Delille <martin@delille.org>
2022-11-14 15:30:28 +0100

    Improve nvim setup

--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,12 @@
-mkdir -Force -p ~/vimfiles/autoload
 New-Item -Force -ItemType SymbolicLink -Path ~/vimfiles/autoload/plug.vim -Value $PSScriptRoot/vendors/vim-plug/plug.vim
 New-Item -Force -ItemType SymbolicLink -Path ~/.vimrc -Value $PSScriptRoot/vimrc
+New-Item -Force -ItemType SymbolicLink -Path "$Env:XDG_CONFIG_HOME/nvim/init.vim" -Value $PSScriptRoot/vimrc
+iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    ni "$Env:XDG_CONFIG_HOME/nvim/autoload/plug.vim" -Force
 New-Item -Force -ItemType SymbolicLink -Path ~/.gitconfig -Value $PSScriptRoot/git/windows
 New-Item -Force -ItemType SymbolicLink -Path $Env:XDG_CONFIG_HOME/git -Value $PSScriptRoot/git
 
 scoop bucket add extras
-scoop install git-aliases posh-git diff-so-fancy make
+scoop install git-aliases posh-git diff-so-fancy make nvim
 
-vim +PlugInstall +PlugUpdate +qall
+vi +PlugInstall +PlugUpdate +qall
