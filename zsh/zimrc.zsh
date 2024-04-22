d5cbda7ba1a98b93bdacbf90cc2166fe2681e265
Martin Delille <martin@delille.org>
2024-04-22 17:31:56 +0200

    [zsh] Move to zimfw plugin manager and powerlevel10k

---
+++ zsh/zimrc.zsh
@@ -0,0 +1,14 @@
+zmodule romkatv/powerlevel10k --use degit
+zmodule ohmyzsh/ohmyzsh --root plugins/1password
+zmodule ohmyzsh/ohmyzsh --root plugins/macos
+zmodule ohmyzsh/ohmyzsh --root plugins/git
+zmodule ohmyzsh/ohmyzsh --root plugins/web-search
+
+zmodule zsh-users/zsh-syntax-highlighting
+zmodule zsh-users/zsh-completions
+zmodule zsh-users/zsh-history-substring-search
+
+zmodule kutsan/zsh-system-clipboard
+zmodule lukechilds/zsh-nvm
+zmodule mattberther/zsh-pyenv
+zmodule ajeetdsouza/zoxide
