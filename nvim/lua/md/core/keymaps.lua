99cf3702fcae879275fe5a3da42a81f729969374
Martin Delille <martin@delille.org>
2024-05-07 23:51:23 +0200

    [nvim] Minor improvement

--- nvim/lua/md/core/keymaps.lua
+++ nvim/lua/md/core/keymaps.lua
@@ -30,3 +30,7 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- buffer management
+keymap.set("n", "<tab>", "<cmd>bn<CR>", { desc = "Go to next buffer" })
+keymap.set("n", "<S-tab>", "<cmd>bp<CR>", { desc = "Go to previous buffer" })
