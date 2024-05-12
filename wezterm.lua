6abd449b7b064aafa549e2fd9db4165f7621abe4
Martin Delille <martin@delille.org>
2024-05-12 17:23:40 +0200

    [wezterm] First setup

---
+++ wezterm.lua
@@ -0,0 +1,50 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = "DanQing Light (base16)"
+config.color_scheme = "Batman"
+config.color_scheme = "DanQing (base16)"
+config.color_scheme = "Darcula (base16)"
+config.color_scheme = "OneHalfDark"
+config.color_scheme = "OneHalfLight"
+config.color_scheme = "Catppuccin Frappe"
+config.automatically_reload_config = true
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 12
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.75
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 8
+
+-- Use the defaults as a base
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+-- make username/project paths clickable. this implies paths like the following are for github.
+-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+-- as long as a full url hyperlink regex exists above this it should not match a full url to
+-- github or gitlab / bitbucket (i.e. https://gitlab.com/user/project.git is still a whole clickable url)
+table.insert(config.hyperlink_rules, {
+	regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+	format = "https://www.github.com/$1/$3",
+})
+
+config.mouse_bindings = {
+	-- Ctrl-click will open the link under the mouse cursor
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	},
+}
+
+-- and finally, return the configuration to wezterm
+return config
