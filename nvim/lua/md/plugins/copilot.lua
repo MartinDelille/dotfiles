eb0c222d8136b32ea0c3c88be157c3433637900b
Martin Delille <martin@delille.org>
2024-10-31 15:26:13 +0100

    [nvim] Add Copilot and CopilotChat plugins

---
+++ nvim/lua/md/plugins/copilot.lua
@@ -0,0 +1,45 @@
+return {
+	{
+		"github/copilot.vim",
+		config = function()
+			vim.g.copilot_filetypes = {
+				["gitcommit"] = true,
+			}
+		end,
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "github/copilot.vim" },
+			{ "nvim-lua/plenary.nvim" },
+		},
+		build = "make tiktoken",
+		opts = {
+			debug = true,
+			mappings = {
+				complete = {
+					detail = "Use @<Tab> or /<Tab> for options.",
+					insert = "<S-Tab>",
+				},
+				reset = {
+					normal = "<C-r>",
+					insert = "<C-r>",
+				},
+			},
+		},
+		keys = {
+			{ "<leader>ccc", ":CopilotChat<CR>", desc = "Open Copilot Chat" },
+			{
+				"<leader>ccq",
+				function()
+					local input = vim.fn.input("Quick Chat: ")
+					if input ~= "" then
+						require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+					end
+				end,
+				desc = "CopilotChat - Quick Chat",
+			},
+		},
+	},
+}
