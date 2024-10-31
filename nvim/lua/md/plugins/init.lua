df9cde9883aaffd128212cb179d85dcf131d5504
Martin Delille <martin@delille.org>
2024-10-31 15:03:51 +0100

    [nvim] Add norcalli/nvim-colorizer

--- nvim/lua/md/plugins/init.lua
+++ nvim/lua/md/plugins/init.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
 	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	"norcalli/nvim-colorizer.lua",
 	"rhysd/conflict-marker.vim",
 }
