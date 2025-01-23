e8acad2edb65aa3ced3b00369ee2b406ed6ab760
Martin Delille <martin@delille.org>
2025-01-23 14:14:33 +0100

    [nvim] Load current-theme.lua only if it exists

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,7 @@
 require("md/core")
 require("md/lazy")
 require("md/themes")
-require("current-theme")
+local current_theme_path = vim.fn.stdpath("config") .. "/lua/current-theme.lua"
+if vim.fn.filereadable(current_theme_path) == 1 then
+	require("current-theme")
+end
