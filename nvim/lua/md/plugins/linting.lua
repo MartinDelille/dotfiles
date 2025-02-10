803d85fdc611056b0f4b2931e31f99872cbfe732
Martin Delille <martin@delille.org>
2025-02-10 21:41:15 +0100

    [lua] Add .stylua.toml

--- nvim/lua/md/plugins/linting.lua
+++ nvim/lua/md/plugins/linting.lua
@@ -14,17 +14,13 @@ return {
 
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
-			callback = function()
-				lint.try_lint()
-			end,
+			callback = function() lint.try_lint() end,
 		})
 
 		-- Set pylint to work in virtualenv
 		require("lint").linters.pylint.cmd = "python"
 		require("lint").linters.pylint.args = { "-m", "pylint", "-f", "json" }
 
-		vim.keymap.set("n", "<leader>l", function()
-			lint.try_lint()
-		end, { desc = "Trigger linting for current file" })
+		vim.keymap.set("n", "<leader>l", function() lint.try_lint() end, { desc = "Trigger linting for current file" })
 	end,
 }
