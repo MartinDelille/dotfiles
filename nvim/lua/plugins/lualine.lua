eb2ecfe625bd44f56378fb58efd8a468ac90305b
Martin Delille <martin@delille.org>
2025-08-21 16:34:19 +0200

    [nvim] Show full file path in lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,6 +3,16 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local lualine = require("lualine")
-		lualine.setup({})
+		lualine.setup({
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+						shorting_target = 40,
+					},
+				},
+			},
+		})
 	end,
 }
