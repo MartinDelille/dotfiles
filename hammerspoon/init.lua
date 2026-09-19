8e7b37d349671abfe770b2518a1ca56a1175eba2
Martin Delille <martin@delille.org>
2026-09-19 13:17:05 +0200

    fixup! [hammerspoon] Add configuration

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,6 @@
-hs.hotkey.bind({}, "F16", function()
-	hs.task.new("/opt/homebrew/bin/m1ddc", nil, { "display", "1", "set", "input", "16" }):start()
-end)
-hs.hotkey.bind({ "cmd", "shift", "alt" }, "R", function()
-	hs.reload()
-end)
+hs.hotkey.bind(
+	{},
+	"F16",
+	function() hs.task.new("/opt/homebrew/bin/m1ddc", nil, { "display", "1", "set", "input", "16" }):start() end
+)
+hs.hotkey.bind({ "cmd", "shift", "alt" }, "R", function() hs.reload() end)
